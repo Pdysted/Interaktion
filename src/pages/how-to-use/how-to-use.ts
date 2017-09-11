fb323a4e6330c93a4a254e9ef23fd05ed7df4775
Pdysted <pdysted@gmail.com>
2017-09-11 18:06:25 +0200

    Added new color schemes as global sass variables and modified the card design

--- src/pages/how-to-use/how-to-use.ts
+++ src/pages/how-to-use/how-to-use.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { HomePage } from '../home/home';
+import { SelectRole } from '../select-role/select-role';
+import { PickACard } from '../pick-a-card/pick-a-card';
+
 @Component({
   selector: 'page-how-to-use',
   templateUrl: 'how-to-use.html'
@@ -10,5 +14,19 @@ export class HowToUse {
   constructor(public navCtrl: NavController) {
 
   }
+
+  selectRolePressed() {
+    this.navCtrl.setRoot(SelectRole);
+  }
+
+  pickACardPressed() {
+    this.navCtrl.setRoot(PickACard);
+  }
+
+  backPressed() {
+    this.navCtrl.setRoot(HomePage);
+  }
+
+
 
 }
