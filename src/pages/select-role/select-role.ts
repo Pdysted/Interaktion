ac1e764b65ee97d5bf0fadc0401b745f905c3cab
Pdysted <pdysted@gmail.com>
2017-10-03 15:25:38 +0200

    Email is partly working needs some tweaks and adjustments, more or less user test ready

--- src/pages/select-role/select-role.ts
+++ src/pages/select-role/select-role.ts
@@ -27,13 +27,13 @@ export class SelectRole {
       if(available) {
         let email = {
           to: 'max@mustermann.de',
-          cc: 'erika@mustermann.de',
+          cc: '',
           bcc: ['john@doe.com', 'jane@doe.com'],
           attachments: [
-            'file://img/logo.png',
-            'res://icon.png',
-            'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
-            'file://README.pdf'
+            //'file://img/logo.png',
+            //'res://icon.png',
+            //'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+            //'file://README.pdf'
           ],
           subject: 'Cordova Icons',
           body: 'How are you? Nice greetings from Leipzig',
@@ -43,6 +43,22 @@ export class SelectRole {
         this.emailComposer.open(email);
       }
      });
+     let email = {
+      to: 'max@mustermann.de',
+      cc: '',
+      bcc: ['john@doe.com', 'jane@doe.com'],
+      attachments: [
+        //'file://img/logo.png',
+        //'res://icon.png',
+        //'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+        //'file://README.pdf'
+      ],
+      subject: 'Cordova Icons',
+      body: 'How are you? Nice greetings from Leipzig',
+      isHtml: true
+    };
+     this.emailComposer.open(email);
+     
   }
 
   openAboutTheRoles() {
