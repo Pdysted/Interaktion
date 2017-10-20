c8adf132cb148e4cfcebf1a1e3411f94e7922b48
Pdysted <pdysted@gmail.com>
2017-10-20 23:47:00 +0200

    Email us in contact

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -5,6 +5,7 @@ import { HomePage } from '../home/home';
 import { HowToUse } from '../how-to-use/how-to-use';
 
 import { CallNumber } from '@ionic-native/call-number';
+import { EmailComposer } from '@ionic-native/email-composer';
 
 import { ToastController } from 'ionic-angular';
 
@@ -15,7 +16,7 @@ import { ToastController } from 'ionic-angular';
 })
 export class Contact {
 
-  constructor(public navCtrl: NavController, public callNumber: CallNumber, public toastCtrl: ToastController) {
+  constructor(public navCtrl: NavController, private emailComposer:EmailComposer, public callNumber: CallNumber, public toastCtrl: ToastController) {
 
   }
 
@@ -25,6 +26,24 @@ export class Contact {
 
   backPressed() {
     this.navCtrl.setRoot(HomePage);
+  }
+
+  emailInteraktion() {
+    let email = {
+      to: 'info@interaktion.dk',
+      cc: '',
+      bcc: [],
+      attachments: [
+        //'file://img/logo.png',
+        //'res://icon.png',
+        //'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+        //'file://README.pdf'
+      ],
+      subject: 'Interaktion app',
+      body: '',
+      isHtml: true
+    };
+     this.emailComposer.open(email);
   }
 
   dialNumber() {
